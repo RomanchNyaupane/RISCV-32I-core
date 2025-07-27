068f8286b4e9d0ff3aaefcd06bc1040f1ac74b30
Romanch <romanchneupane53@gmail.com>
2025-07-27 19:10:35 +0545

    added branch instruction

--- RISC_V_RV32I/control_unit.v
+++ RISC_V_RV32I/control_unit.v
@@ -1,166 +1,212 @@
-// the control unit is state machine based instead of microcode based that i was used to
+`include "ALU.v";
+`include "memory_data_register.v";
+`include "data_memory.v";
+`include "register_bank.v";
+`include "memory_address_register.v";
+`include "instruction_register.v";
+`include "instruction_memory.v";
+`include "instruction_counter.v"
+`include "control_unit.v"
+`include "imm_gen.v"
+`include "mux_1.v"
+`include "mux_2.v"
+`include "mux_3.v"
+`include "demux_1.v"
+`include "reg_bank_address_register.v"
+`include "branch_comparator.v"
 
-/*
-RISC V instruction decoding
-R instruction: [31:25] - funct7
-               [24:20] - rs2 address(source register)
-               [19:15] - rs1 address
-               [14:12] - funct3
-               [11:7] - rd address (destination register)
-               [6:0] - opcode
-*/
-module control_unit(instr_in, ctrl_clk, ctrl_rst,carry_in, zero_in,alu_opcode_wr_en, mem_wr_en,
-                    alu_opcode, ir_wr_en, ic_count, ic_dir, imm_gen_instr_wr_en,
-                    mdr_rd_en, mux_1_sel, mux_2_sel, mux_3_sel, demux_1_sel, instr_type, reg_wr_en, 
-                    mar_wr_en, reg_rs_1_addr_wr_en,reg_rs_2_addr_wr_en, reg_rd_addr_wr_en);
-
-input wire[31:0] instr_in;
-input wire ctrl_clk, ctrl_rst;
-input wire carry_in, zero_in;
-
-output reg [3:0] alu_opcode;
-output reg ir_wr_en, ic_count, reg_wr_en, ic_dir, alu_opcode_wr_en, mem_wr_en;
-//output reg [3:0] instr_type;
-output reg mdr_rd_en, mar_wr_en, imm_gen_instr_wr_en;
-output wire reg_rs_1_addr_wr_en, reg_rs_2_addr_wr_en, reg_rd_addr_wr_en;
-
-parameter state_1  = 4'd1, state_2  = 4'd2, state_3  = 4'd3, state_4  = 4'd4, state_5  = 4'd5;
-parameter state_6  = 4'd6, state_7  = 4'd7, state_8  = 4'd8, state_9  = 4'd9, state_10 = 4'd10;
-parameter state_11 = 4'd11, state_12 = 4'd12, state_13 = 4'd13, state_14 = 4'd14, state_15 = 4'd15;
-parameter state_16 = 4'd0;
-
-wire [6:0] opcode = instr_in[6:0];
-wire [2:0] funct3 = instr_in[14:12];
-wire [6:0] funct7 = instr_in[31:25];
-
-parameter R_type = 4'd1, I_type_1 = 4'd2, I_type_2 = 4'd3, I_type_3=4'd4, I_type_4=4'd5;
-parameter S_type = 4'd6, B_type = 4'd7, U_type = 4'd8, J_type = 4'd9;
-
-output wire [3:0] instr_type;
-assign instr_type = (opcode == 7'b0110011)? R_type :
-                          (opcode == 7'b0010011)? I_type_1 :
-                          (opcode == 7'b0000011)? I_type_2:
-                          (opcode == 7'b1100111 & funct3 == 3'b000)? I_type_3:
-                          (opcode == 7'b1110011 & funct3 == 3'b000)? I_type_4:                          
-                          (opcode == 7'b0100011)? S_type :
-                          (opcode == 7'b1100011)? B_type :
-                          (opcode == 7'b1100111 & funct3 != 3'b000)? J_type:
-                          (opcode == 7'b0110111 | 7'b0010111)? U_type : 4'b0;
-
-wire rs_1_addr_wr_en, rs_2_addr_wr_en, rd_addr_wr_en;
-wire is_r_instr, is_i_instr, is_s_instr, is_b_instr, is_j_instr, is_u_instr;
+module module_connect(main_clk, main_rst);
+input wire main_clk;
+input wire main_rst;
 
 
-assign is_r_instr = (instr_type == R_type) ? 1 :0;
-assign is_i_instr = (instr_type == I_type_1 | instr_type == I_type_2 | instr_type == I_type_3 | instr_type == I_type_4);
-assign is_s_instr = (instr_type == S_type)? 1 : 0;
-assign is_b_instr = (instr_type == B_type)? 1 : 0;
-assign is_j_instr = (instr_type == J_type)? 1 : 0;
-assign is_u_instr = (instr_type == U_type)? 1 : 0;
+// ? dp- data paths , ctrl- control lines
+// only output names of modules are used to name wires connecting any ports. if output of A is connected to input of B, the wire common to
+// both will be named after output of A
 
-assign reg_rs_1_addr_wr_en = (is_r_instr | is_i_instr | is_s_instr | is_b_instr);
-assign reg_rs_2_addr_wr_en = (is_r_instr | is_s_instr | is_b_instr);
-assign reg_rd_addr_wr_en = (is_r_instr | is_i_instr | is_u_instr | is_j_instr);
+//bus
+wire [31:0] dp_bus_1;
 
-output wire demux_1_sel, mux_1_sel, mux_2_sel;
-output wire [1:0] mux_3_sel;
-reg rs_2_out_en, rs_1_out_en, pc_out_en, alu_out_en;
+//register bank
+wire [31:0] dp_reg_out_1, dp_reg_out_2;
 
-assign mux_1_sel = (rs_2_out_en) ? 0 : 1; // to toggle mux1
-assign mux_2_sel = (rs_1_out_en) ? 0 : 1; // to toggle mux2
-assign demux_1_sel = (mar_wr_en == 1) ? 0 : 1;
-assign mux_3_sel = (alu_out_en == 1) ? 2'b00 : (mdr_rd_en == 1) ? 2'b01 : (pc_out_en == 1) ? 2'b10 : 11;
+//alu
+wire [31:0] dp_alu_out;
+
+//memory
+wire [31:0] dp_memory_out, dp_mdr_out;
+wire [31:0] dp_mar_out;
+
+// register bank address outs
+wire [4:0] dp_reg_bank_rs_1_addr;
+wire [4:0] dp_reg_bank_rs_2_addr;
+wire [4:0] dp_reg_bank_rd_addr;
 
 
+//instruction counter and registers
+wire [31:0] dp_instr_mem_out, dp_ic_out, dp_ir_out;
 
-reg [3:0] state, next_state; // this can cover 16 state which i think will be sufficient
-//reg[4:0] reg_rd_addr_1, reg_rd_addr_2, reg_wr_addr;
-//reg carry_in, zero_in, reg_wr_en;
+//mux and demux
+wire [31:0] dp_mux_1_out, dp_mux_2_out, dp_mux_3_out, dp_demux_1_out_0, dp_demux_1_out_1;
 
-always @(posedge ctrl_clk) begin
-    if(ctrl_rst) state <= state_1; else state <= next_state;
-end
-always @(state, opcode, instr_in) begin            // always block not under clock so blocking statements used
-    ic_count = 0; ir_wr_en = 0; reg_wr_en = 0;
-    //instr_type = 0;
-    mdr_rd_en = 0; ic_dir = 0;
-    alu_opcode = 0; mar_wr_en = 0; alu_opcode_wr_en=0; imm_gen_instr_wr_en=0;
-    mem_wr_en = 0;
-    rs_2_out_en = 1; rs_1_out_en = 1; alu_out_en = 0; pc_out_en = 0;
+//imm_gen
+wire [31:0] dp_imm_gen_out;
 
-    //reg_rs_1_addr_wr_en = rs_1_addr_wr_en; reg_rs_2_addr_wr_en = rs_2_addr_wr_en; reg_rd_addr_wr_en = rd_addr_wr_en;
-    case(state)
-        state_1: next_state<=state_2;
-        state_2: begin  //fetch instruction
-            next_state = state_3; 
-            ir_wr_en = 1;
-            ic_count = 1;
-        end
-        state_3: begin  //decode
-            case({opcode, funct3, funct7})  
-                {7'b0110011, 3'b000, 7'h00}: begin alu_opcode = 4'b0001; next_state=state_4; alu_opcode_wr_en=1; end //add operation
-                {7'b0110011, 3'b000, 7'h20}: begin alu_opcode = 4'b0010; next_state=state_4; alu_opcode_wr_en=1; end //sub operation
-                {7'b0110011, 3'b100, 7'h00}: begin alu_opcode = 4'b0011; next_state=state_4; alu_opcode_wr_en=1; end //xor
-                {7'b0110011, 3'b110, 7'h00}: begin alu_opcode = 4'b0100; next_state=state_4; alu_opcode_wr_en=1; end //or
-                {7'b0110011, 3'b111, 7'h00}: begin alu_opcode = 4'b0101; next_state=state_4; alu_opcode_wr_en=1; end //and
-                {7'b0110011, 3'b001, 7'h00}: begin alu_opcode = 4'b0110; next_state=state_4; alu_opcode_wr_en=1; end //sll
-                {7'b0110011, 3'b101, 7'h00}: begin alu_opcode = 4'b0111; next_state=state_4; alu_opcode_wr_en=1; end //srl
-                {7'b0110011, 3'b101, 7'h20}: begin alu_opcode = 4'b1000; next_state=state_4; alu_opcode_wr_en=1; end //sra
-                {7'b0110011, 3'b010, 7'h00}: begin alu_opcode = 4'b1001; next_state=state_4; alu_opcode_wr_en=1; end //slt
-                {7'b0110011, 3'b011, 7'h00}: begin alu_opcode = 4'b1010; next_state=state_4; alu_opcode_wr_en=1; end //stlu
+// branch comparator
 
-                {7'b0000011, 3'b010, 7'h00}: begin //load word(lw)
-                    //instr_type = instruction_type;  //instr_type is input to imm_gen
-                    imm_gen_instr_wr_en = 1;
-                    next_state = state_5;
-                end
-                {7'b0100011, 3'b010, 7'h00}: begin //store word(lw)
-                    //instr_type = instruction_type;  //instr_type is input to imm_gen
-                    imm_gen_instr_wr_en = 1;
-                    next_state = state_7;
-                    rs_2_out_en = 1;
-                    rs_1_out_en = 0;
-                    alu_out_en = 1;
-                    alu_opcode = 4'b0001;
-                    alu_opcode_wr_en = 1;
-                    mar_wr_en = 1;
-                    
-                end
-                default: next_state = state_8;
-                    
-            endcase
 
-        end
-        state_4: begin  //execution of R instructions
-            rs_2_out_en = 1;
-            rs_1_out_en = 1;
-            alu_out_en = 1;
-            reg_wr_en = 1;
-            ic_count = 1;
-            next_state = state_2;
-        end
-        state_5: begin  //execution of load instruction(i_type_1)
-            rs_2_out_en = 1;
-            rs_1_out_en = 0;
-            alu_out_en = 1;
-            alu_opcode = 4'b0001;
-            alu_opcode_wr_en = 1;
-            mar_wr_en = 1;
-            next_state = state_6; //address provided to memory. time to read memory
-        end
-        state_6: begin //final step of load_instruction execution
-            mdr_rd_en = 1;
-            reg_wr_en = 1;
-            next_state = state_2;
-        end
-        state_7: begin
-            mem_wr_en = 1;
-            next_state = state_2;
-        end
-        state_8: next_state = state_8;//halt
-            
+wire ctrl_mem_wr_en, ctrl_reg_wr_en;
+wire ctrl_mem_rd_en; // actually it is mdr_rd_en but giving mem_rd_en makes it more intuitive in terms of memory reading
+wire ctrl_mar_wr_en, ctrl_ir_wr_en, ctrl_ic_dir, ctrl_ic_cnt, ctrl_instr_mem_wr_en;
+wire ctrl_reg_rs_1_addr_wr_en, ctrl_reg_rs_2_addr_wr_en, ctrl_reg_rd_addr_wr_en;
+wire ctrl_mux_1_sel , ctrl_mux_2_sel, ctrl_demux_1_sel, ctrl_bc_en, ctrl_bc_out;
+wire [1:0] ctrl_mux_3_sel;
+wire [3:0] ctrl_instr_type, dp_alu_opcode;
 
-    endcase
-end
+alu alu_inst(
+    .alu_in_2(dp_mux_2_out),
+    .alu_in_1(dp_mux_1_out),
+    .alu_out(dp_alu_out),
+    .alu_opcode(dp_alu_opcode),
+    .alu_carry(ctrl_carry_out),
+    .alu_zero(ctrl_zero_out)
+);
+
+data_memory data_mem_inst(
+    .data_mem_in(dp_reg_out_2),
+    .data_mem_out(dp_memory_out),
+    .data_mem_addr(dp_mar_out),
+    .data_mem_clk(main_clk),
+    .data_mem_wr_en(ctrl_mem_wr_en)
+);
+
+register_bank reg_bank_inst(
+    .reg_rd_addr_1(dp_reg_bank_rs_1_addr),
+    .reg_rd_addr_2(dp_reg_bank_rs_2_addr),
+    .reg_wr_addr(dp_reg_bank_rd_addr),
+    .reg_wr_en(ctrl_reg_wr_en),
+    .reg_in(dp_mux_3_out),
+    .reg_out_1(dp_reg_out_1),
+    .reg_out_2(dp_reg_out_2),
+    .reg_clk(main_clk),
+    .reg_rst(main_rst)
+);
+
+memory_data_register mdr_inst(
+    .mdr_in(dp_memory_out),
+    .mdr_out(dp_mdr_out),
+    .mdr_rd(ctrl_mem_rd_en),
+    .mdr_rst(main_rst),
+    .mdr_clk(main_clk)
+);
+
+memory_address_register mar_inst(
+    .mar_in(dp_demux_1_out_0),
+    .mar_out(dp_mar_out),
+    .mar_wr_en(ctrl_mar_wr_en),
+    .mar_clk(main_clk),
+    .mar_rst(main_rst)
+);
+
+instruction_counter ic_inst(
+    .ic_out(dp_ic_out),
+    .ic_in(dp_alu_out[4:0]),
+    .ic_clk(main_clk),
+    .ic_rst(main_rst),
+    .ic_dir(ctrl_ic_dir),
+    .ic_cnt(ctrl_ic_cnt),
+    .ic_wr_en(ctrl_ic_wr_en)
+);
+instruction_memory instr_mem_inst(
+    .instr_mem_out(dp_instr_mem_out),
+    .instr_mem_addr(dp_ic_out),
+    .instr_mem_clk(main_clk)
+);
+instruction_register instr_reg_inst(
+    .ir_in(dp_instr_mem_out),
+    .ir_out(dp_ir_out),
+    .ir_wr_en(ctrl_ir_wr_en),
+    .ir_rst(main_rst),
+    .ir_clk(main_clk)
+);
+control_unit control_unit_inst(
+    .ctrl_clk(main_clk), 
+    .instr_in(dp_ir_out),
+    .ctrl_rst(main_rst),
+    .carry_in(ctrl_carry_out), 
+    .zero_in(ctrl_zero_out),
+    .alu_opcode(dp_alu_opcode), 
+    .ir_wr_en(ctrl_ir_wr_en),
+    .ic_count(ctrl_ic_cnt),
+    .mdr_rd_en(ctrl_mem_rd_en),
+    .mux_1_sel(ctrl_mux_1_sel), 
+    .mux_2_sel(ctrl_mux_2_sel),
+    .mux_3_sel(ctrl_mux_3_sel),
+    .demux_1_sel(ctrl_demux_1_sel),
+    .instr_type(ctrl_instr_type),
+    .reg_wr_en(ctrl_reg_wr_en),
+    .mar_wr_en(ctrl_mar_wr_en),
+    .ic_dir(ctrl_ic_dir),
+    .ic_wr_en(ctrl_ic_wr_en),
+    .bc_en(ctrl_bc_en),
+    .bc_in(ctrl_bc_out),
+    .imm_gen_instr_wr_en(ctrl_imm_gen_instr_wr_en),
+    .reg_rs_1_addr_wr_en(ctrl_reg_rs_1_addr_wr_en),
+    .reg_rs_2_addr_wr_en(ctrl_reg_rs_2_addr_wr_en),
+    .reg_rd_addr_wr_en(ctrl_reg_rd_addr_wr_en),
+    .mem_wr_en(ctrl_mem_wr_en)
+);
+imm_gen imm_gen_inst(
+    .imm_gen_in(dp_ir_out),
+    .imm_gen_instr_type(ctrl_instr_type),
+    .imm_gen_out(dp_imm_gen_out),
+    .imm_gen_instr_wr_en(ctrl_imm_gen_instr_wr_en)
+);
+reg_bank_address_register reg_bank_addr_reg_inst(
+    .rs_1_in(dp_ir_out[19:15]), 
+    .rs_2_in(dp_ir_out[24:20]), 
+    .rd_in(dp_ir_out[11:7]),
+    .rs_1_wr_en(ctrl_reg_rs_1_addr_wr_en), 
+    .rs_2_wr_en(ctrl_reg_rs_2_addr_wr_en), 
+    .rd_wr_en(ctrl_reg_rd_addr_wr_en), 
+    .reg_clk(main_clk),
+    .reg_rst(main_rst),
+    .rs_1_out(dp_reg_bank_rs_1_addr), 
+    .rs_2_out(dp_reg_bank_rs_2_addr), 
+    .rd_out(dp_reg_bank_rd_addr)
+);
+mux_1 mux_1_inst(
+    .mux_1_in0(dp_reg_out_1),
+    .mux_1_in1(dp_ic_out),
+    .mux_1_sel(ctrl_mux_1_sel),
+    .mux_1_out(dp_mux_1_out)
+);
+mux_2 mux_2_inst(
+    .mux_2_in0(dp_reg_out_2),
+    .mux_2_in1(dp_imm_gen_out),
+    .mux_2_sel(ctrl_mux_2_sel),
+    .mux_2_out(dp_mux_2_out)
+);
+mux_3 mux_3_inst(
+    .mux_3_in0(dp_demux_1_out_1),
+    .mux_3_in1(dp_mdr_out),
+    .mux_3_in2(dp_ic_out),
+    .mux_3_sel(ctrl_mux_3_sel),
+    .mux_3_out(dp_mux_3_out)
+);
+demux_1 demux_1_inst(
+    .demux_in(dp_alu_out),
+    .demux_sel(ctrl_demux_1_sel),
+    .demux_out0(dp_demux_1_out_0),
+    .demux_out1(dp_demux_1_out_1)
+);
+
+branch_comparator bc_inst(
+    .bc_in_1(dp_reg_out_1),
+    .bc_in_2(dp_reg_out_2),
+    .bc_en(ctrl_bc_en),
+    .bc_out(ctrl_bc_out),
+    .bc_opcode(dp_ir_out[14:12])
+);
 
 endmodule
