3bece487cac060c8b59d87349e74ebcaaab1cc8b
Romanch <romanchneupane53@gmail.com>
2025-07-29 22:19:35 +0545

    switched to ROM based intruction memory using case statement

--- RISC_V_RV32I/instruction_memory.v
+++ RISC_V_RV32I/instruction_memory.v
@@ -1,70 +1,23 @@
-// 32 bit in and 32 bit out. 16 bit support not included currently
 module instruction_memory #(
-    parameter MEMORY_DEPTH = 8,// 8 bits deep
-    parameter ADDRESS_WIDTH = 32, //32 bit addressing
-    parameter SIZE = 16'h0FFF
+    parameter ADDRESS_WIDTH = 5  // 2^5 = 32 addresses max
 )(
-    instr_mem_out, instr_mem_addr, instr_mem_clk
+    output reg [31:0] instr_mem_out,
+    input wire [ADDRESS_WIDTH-1:0] instr_mem_addr,
+    input wire instr_mem_clk
 );
-input wire [4:0] instr_mem_addr;
-input wire instr_mem_clk;
 
-output reg [31:0] instr_mem_out;
-
-
-reg [MEMORY_DEPTH-1:0] instr_mem_bank_3 [0: SIZE - 1];
-reg [MEMORY_DEPTH-1:0] instr_mem_bank_2 [0: SIZE - 1];
-reg [MEMORY_DEPTH-1:0] instr_mem_bank_1 [0: SIZE - 1];
-reg [MEMORY_DEPTH-1:0] instr_mem_bank_0 [0: SIZE - 1];
-
-always @(posedge instr_mem_clk) begin
-    instr_mem_bank_3[0] <= 8'b0;         // load at register 0(memory read)
-    instr_mem_bank_2[0] <= 8'b1;
-    instr_mem_bank_1[0] <= 8'b10100000;
-    instr_mem_bank_0[0] <= 8'b11;
-    
-    instr_mem_bank_3[1] <= 8'b0;         // load at register 1 (memory read)
-    instr_mem_bank_2[1] <= 8'b00010001;
-    instr_mem_bank_1[1] <= 8'b10100000;
-    instr_mem_bank_0[1] <= 8'b10000011;
-    
-    instr_mem_bank_3[2] <= 8'b0;         // add and store at destination register
-    instr_mem_bank_2[2] <= 8'b0;
-    instr_mem_bank_1[2] <= 8'b10000001;
-    instr_mem_bank_0[2] <= 8'b00110011;
-
-    instr_mem_bank_3[3] <= 8'b0;        // store register at memory (memory write)
-    instr_mem_bank_2[3] <= 8'b00100001;
-    instr_mem_bank_1[3] <= 8'b10100001;
-    instr_mem_bank_0[3] <= 8'b00100011;
-
-    instr_mem_bank_3[4] <= 8'b00000000; // add immediate rd = rs1 + imm. loads 09 to register 5
-    instr_mem_bank_2[4] <= 8'b10010001;
-    instr_mem_bank_1[4] <= 8'b10000010;
-    instr_mem_bank_0[4] <= 8'b00010011;
-
-    instr_mem_bank_3[5] <= 8'b01000000;// shift right arithmetic
-    instr_mem_bank_2[5] <= 8'b01000000;
-    instr_mem_bank_1[5] <= 8'b11010010;
-    instr_mem_bank_0[5] <= 8'b10110011;
-    
-    instr_mem_bank_3[6] <= 8'b0;       // branch at rs1 != rs2 to memory location 14
-    instr_mem_bank_2[6] <= 8'b0;
-    instr_mem_bank_1[6] <= 8'b10010100;
-    instr_mem_bank_0[6] <= 8'b01100011;
-    
-    instr_mem_bank_3[14] <= 8'b0;     // add immediate rd = rs1 + imm. loads 0b in register 5
-    instr_mem_bank_2[14] <= 8'b10110001;
-    instr_mem_bank_1[14] <= 8'b10000010;
-    instr_mem_bank_0[14] <= 8'b00010011;
-
-    instr_mem_out <={
-                    instr_mem_bank_3[instr_mem_addr], 
-                    instr_mem_bank_2[instr_mem_addr], 
-                    instr_mem_bank_1[instr_mem_addr], 
-                    instr_mem_bank_0[instr_mem_addr]
-                   };
-    
+always @(instr_mem_addr) begin
+    case (instr_mem_addr)
+        5'd0:  instr_mem_out <= 32'b00000000_00000001_10100000_00000011; // load at register 0(memory read)
+        5'd1:  instr_mem_out <= 32'b00000000_00010001_10100000_10000011; // load at register 1 (memory read)
+        5'd2:  instr_mem_out <= 32'b00000000_00000000_10000001_00110011; // add and store at destination register
+        5'd3:  instr_mem_out <= 32'b00000000_00100001_10100001_00100011; // store register at memory (memory write)
+        5'd4:  instr_mem_out <= 32'b00000000_10010001_10000010_00010011; // add immediate rd = rs1 + imm. loads 09 to register 5
+        5'd5:  instr_mem_out <= 32'b01000000_01000000_11010010_10110011; // shift right arithmetic
+        5'd6:  instr_mem_out <= 32'b00000000_00000000_10010100_01100011; // branch at rs1 != rs2 to memory location 14
+        5'd14: instr_mem_out <= 32'b00000000_10110001_10000010_00010011; // add immediate rd = rs1 + imm. loads 0b in register 5
+        default: instr_mem_out <= 32'b0; 
+    endcase
 end
 
-endmodule;
+endmodule
